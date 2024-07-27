61354e8fab6e5df214deda366bd4f6cb294a9ad5
aurelia ritter <aureliaritter@gmail.com>
2024-07-27 17:46:17 +0400

    Update pet

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -17,6 +17,16 @@ class PetsController < ApplicationController
     redirect_to pet_path(@pet)
   end
 
+  def edit
+    @pet = Pet.find(params[:id])
+  end
+
+  def update
+    @pet = Pet.find(params[:id])
+    @pet.update(pet_params)
+    redirect_to pet_path(@pet)
+  end
+
   private
 
   def pet_params
--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,6 @@ Rails.application.routes.draw do
   get 'pets/new', to: 'pets#new'
   post 'pets', to: 'pets#create'
   get 'pets/:id', to: 'pets#show', as: 'pet'
-  
+  get 'pets/:id/edit', to: 'pets#edit', as: 'edit_pet'
+  patch 'pets/:id', to: 'pets#update'
 end
